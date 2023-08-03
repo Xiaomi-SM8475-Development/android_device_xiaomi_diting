9f55673c9d22aac6e2068bdae14ae395fc479aa2
klozz <carlosj@klozz.dev>
2023-08-03 22:21:39 +0000

    diting: build XiaomiEuicc
    Signed-off-by: klozz <carlosj@klozz.dev>

--- device.mk
+++ device.mk
@@ -149,7 +149,8 @@ include $(DEVICE_PATH)/configs/properties/default.mk
 PRODUCT_PACKAGES += \
     Ims \
     QtiTelephony \
-    qti-telephony-common
+    qti-telephony-common \
+    XiaomiEuicc
 
 # Perf
 PRODUCT_PACKAGES += \
