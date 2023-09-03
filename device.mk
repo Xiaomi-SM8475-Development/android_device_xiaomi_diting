2e0f40e2408e038dafd9256cf4694e96ed2f202d
klozz <carlosj@klozz.dev>
2023-09-02 21:00:44 -0600

    diting: declare euicc permission
    Signed-off-by: klozz <carlosj@klozz.dev>

--- device.mk
+++ device.mk
@@ -258,6 +258,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml \
+
 # Update engine
 PRODUCT_PACKAGES += \
     checkpoint_gc \
