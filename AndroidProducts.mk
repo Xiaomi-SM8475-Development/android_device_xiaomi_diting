3a211c381738b4b5640f58a213160d8219a31083
klozz <carlosj@klozz.dev>
2023-09-06 06:35:22 +0000

    havoc bring
    Signed-off-by: klozz <carlosj@klozz.dev>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,13 +5,13 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/derp_diting.mk \
-    $(LOCAL_DIR)/derp_ditingp.mk
+    $(LOCAL_DIR)/havoc_diting.mk \
+    $(LOCAL_DIR)/havoc_ditingp.mk
 
 COMMON_LUNCH_CHOICES := \
-    derp_diting-eng \
-    derp_diting-userdebug \
-    derp_diting-user \
-    derp_ditingp-eng \
-    derp_ditingp-userdebug \
-    derp_ditingp-user
+    havoc_diting-eng \
+    havoc_diting-userdebug \
+    havoc_diting-user \
+    havoc_ditingp-eng \
+    havoc_ditingp-userdebug \
+    havoc_ditingp-user
--- havoc_diting.mk
+++ havoc_diting.mk
@@ -8,15 +8,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common DerpFest stuff.
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+# Inherit some common Havoc stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from diting device.
 $(call inherit-product, device/xiaomi/diting/device.mk)
 
 # Rom Stuff
 EXTRA_UDFPS_ANIMATIONS := true
-DERP_BUILDTYPE := Official
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 TARGET_FACE_UNLOCK_SUPPORTED := true
